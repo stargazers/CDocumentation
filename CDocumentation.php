5d60700fde95cf3d1b576d2d19f2c46c7512bec4
Aleksi Räsänen <aleksi.rasanen@runosydan.net>
2011-05-18 03:16:39 +0300

    Bugfix for wrong index.
    + If there were multiple tags with same name, for example two
      @author tags then we wouldn't set index back to zero for tags
      what was defined later in comment block. So if there was two
      @author tags and later in comment block there was tag @email
      or whatever, it got index number 1, not 0 as it should.

--- CDocumentation.php
+++ CDocumentation.php
@@ -166,6 +166,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 					if( isset( $ret[$current_tag][$i] ) )
 						$i++;
+					else
+						$i = 0;
 
 					$line_to_add = substr( $line, 
 						strlen( $tmp[0] ) + 1 ) . ' ';
